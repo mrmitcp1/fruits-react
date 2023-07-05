924c2299871a422a0002aa94805977b81176c5d8
mrmitcp1 <mrmitcp55@gmail.com>
2023-07-05 15:52:50 +0700

    fruits

--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Apricot",
+    "Black rowan",
+    "Cranberry"
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+      <h1>List of fruits</h1>
+      <ul>
+          {fruits.map(item=>
+              <li>{item}</li>
+          )}
+      </ul>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
